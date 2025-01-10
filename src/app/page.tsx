6c41797d1c8544dc14b176059690e99cf1809e6f
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-10 09:10:34 -0800

    Add essential Next.js files

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <h1 className="text-4xl font-bold">BlindBarrels.com</h1>
+      </div>
+
+      <div className="relative flex place-items-center">
+        <p>Welcome to BlindBarrels.com</p>
+      </div>
+    </main>
+  )
+}
