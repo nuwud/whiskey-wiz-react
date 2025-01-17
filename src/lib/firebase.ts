58c56e8411839866963027c1dec5421da00d8004
Patrick Allan Wood <nuwudorder@gmail.com>
2025-01-16 20:48:18 -0800

    Update Firebase config to use environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAnalytics, isSupported } from 'firebase/analytics';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqZMaGpJGf5veYgTXmytg1bLerva-of0U",
-  authDomain: "whiskeywiz2.firebaseapp.com",
-  databaseURL: "https://whiskeywiz2-default-rtdb.firebaseio.com",
-  projectId: "whiskeywiz2",
-  storageBucket: "whiskeywiz2.appspot.com",
-  messagingSenderId: "555320797929",
-  appId: "1:555320797929:web:0d4b062d7f2ab330fc1e78",
-  measurementId: "G-SK0TJJEPF5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
